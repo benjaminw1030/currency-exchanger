a858959e67894177732c2ef5c8961f574e15e756
Ben Wilson <benjaminw1030@gmail.com>
2021-09-10 15:03:14 -0700

    more refactoring for edge cases

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ function reset() {
 function outputExchange(amount, currency1, currency2, response) {
   if (response.result === 'success' && response.conversion_rates[currency2] !== undefined) {
     let outputAmount = (amount * response.conversion_rates[currency2]).toFixed(2);
-    $('#currency-output').text(`This amount of ${currency1} is worth ${outputAmount} ${currency2}`);
+    $('#currency-output').text(`${amount.toFixed(2)} ${currency1} is worth ${outputAmount} ${currency2}`);
   } else if (response.result === 'success' && response.conversion_rates[currency1] === undefined) {
     $('#error-output').text(`There was an error: currency to convert to does not exist.`);
   } else {
@@ -26,21 +26,29 @@ function outputExchange(amount, currency1, currency2, response) {
 $(document).ready(function () {
   $('#currency-exchange-form').submit(function (event) {
     event.preventDefault();
-    let amount = $('#amount').val();
+    let amount = parseFloat($('#amount').val());
     let currency1 = $('#currency-1').val();
     let currency2 = $('#currency-2').val();
     let currencyOther1 = ($('#currency-other-1').val()).toUpperCase();
     let currencyOther2 = ($('#currency-other-2').val()).toUpperCase();
+    if (isNaN(amount)) {
+      amount = 0;
+    }
     if (currencyOther1 !== '') {
       currency1 = currencyOther1;
     }
     if (currencyOther2 !== '') {
       currency2 = currencyOther2;
     }
-    ExchangeRateService.currencyExchange(currency1)
-      .then(function (response) {
-        reset();
-        outputExchange(amount, currency1, currency2, response);
-      });
+    console.log(amount, typeof amount)
+    if (currency1 === '' || currency2 === '') {
+      $('#error-output').text('Please select a currency.');
+    } else {
+      ExchangeRateService.currencyExchange(currency1)
+        .then(function (response) {
+          reset();
+          outputExchange(amount, currency1, currency2, response);
+        });
+    }
   });
 });
