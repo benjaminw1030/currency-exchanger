321adbb6682239acd726aacb97b351e0f61c3786
Ben Wilson <benjaminw1030@gmail.com>
2021-09-10 09:14:13 -0700

    complete ExchaneRateService api call function

--- src/index.js
+++ src/index.js
@@ -4,10 +4,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import ExchangeRateService from './services/exchange-rate-service.js';
 
+function currencyCalc(amount, currency) {
+
+}
+
 $(document).ready(function() {
   $('#currency-exchange-form').submit(function(event) {
     event.preventDefault();
-
+    let amount = $("#USD-amount").val();
+    let currency = $("#currency").val();
+    ExchangeRateService.currencyExchange()
+      .then(function (response) {
+        currencyCalc(response);
+      });
     $('#currency-output').append(`<p></p>`);
   });
 });
--- src/services/exchange-rate-service.js
+++ src/services/exchange-rate-service.js
@@ -1,4 +1,14 @@
 export default class ExchangeRateService {
   static currencyExchange() {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}`)
+      .then(function (response) {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch(function (error) {
+        return error;
+      })
   }
 }
