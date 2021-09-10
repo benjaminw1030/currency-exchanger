8ae66382b3697420941ff7b32c4478623b1e43d1
Ben Wilson <benjaminw1030@gmail.com>
2021-09-10 10:08:51 -0700

    further error handling for invalid currencies

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import './css/styles.css';
 import ExchangeRateService from './services/exchange-rate-service.js';
 
 function outputExchange(amount, currency, response) {
-  if (response.result === 'success') {
+  if (response.result === 'success' && response.conversion_rates[currency] !== undefined) {
   let outputAmount = (amount * response.conversion_rates[currency]).toFixed(2);
   $('#currency-output').text(`This amount of USD is worth ${outputAmount} ${currency}`);
+  } else if (response.result === 'success' && response.conversion_rates[currency] === undefined) {
+    $('#error-output').text(`There was an error: currency does not exist.`)
   } else {
     $('#error-output').text(`There was an error: ${response.message}.`)
   }
