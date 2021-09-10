0edc873d4c9ee43f513971b38d66cf21628163ef
Ben Wilson <benjaminw1030@gmail.com>
2021-09-10 12:13:50 -0700

    WIP: error-handling

--- src/index.js
+++ src/index.js
@@ -6,10 +6,13 @@ import ExchangeRateService from './services/exchange-rate-service.js';
 
 function outputExchange(amount, currency, response) {
   if (response.result === 'success' && response.conversion_rates[currency] !== undefined) {
-  let outputAmount = (amount * response.conversion_rates[currency]).toFixed(2);
-  $('#currency-output').text(`This amount of USD is worth ${outputAmount} ${currency}`);
+    console.log(response.result)
+    let outputAmount = (amount * response.conversion_rates[currency]).toFixed(2);
+    $('#currency-output').text(`This amount of USD is worth ${outputAmount} ${currency}`);
   } else if (response.result === 'success' && response.conversion_rates[currency] === undefined) {
-    $('#error-output').text(`There was an error: currency does not exist.`)
+    $('#error-output').text(`There was an error: currency to obtain does not exist.`)
+  } else if (response.result === 'error' && response['error-type'] === 'unsupported-code') {
+    $('#error-output').text(`There was an error: currency to exchange does not exist.`)
   } else {
     $('#error-output').text(`There was an error: ${response.message}.`)
   }
@@ -18,11 +21,18 @@ function outputExchange(amount, currency, response) {
 $(document).ready(function () {
   $('#currency-exchange-form').submit(function (event) {
     event.preventDefault();
-    let amount = $('#USD-amount').val();
-    let currency = $('#currency').val();
-    ExchangeRateService.currencyExchange()
+    let amount = $('#amount').val();
+    let currency1 = $('#currency-1').val();
+    let currency2 = $('#currency-2').val();
+    if (currency1 === 'other') {
+      currency1 = $('#currency-other-1').val();
+    }
+    if (currency2 === 'other') {
+      currency2 = $('#currency-other-2').val();
+    }
+    ExchangeRateService.currencyExchange(currency1)
       .then(function (response) {
-        outputExchange(amount, currency, response);
+        outputExchange(amount, currency2, response);
       });
   });
 });
--- src/services/exchange-rate-service.js
+++ src/services/exchange-rate-service.js
@@ -1,6 +1,6 @@
 export default class ExchangeRateService {
-  static currencyExchange() {
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+  static currencyExchange(currency) {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currency}`)
       .then(function (response) {
         if (!response.ok) {
           throw Error(response.statusText);
