e016b6617b9eb8ba81dae518190fea4bbdad4d0c
Ben Wilson <benjaminw1030@gmail.com>
2021-09-10 09:56:50 -0700

    add error handling, replace double quotes with single quotes

--- src/index.js
+++ src/index.js
@@ -4,26 +4,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import ExchangeRateService from './services/exchange-rate-service.js';
 
-function findExchangeRate(response, currency) {
-  console.log(response.conversion_rates[currency])
-  return response.conversion_rates[currency]
-}
-
-function outputExchange(amount, exchangeRate, currency) {
-  let outputAmount = (amount * exchangeRate).toFixed(2);
+function outputExchange(amount, currency, response) {
+  if (response.result === 'success') {
+  let outputAmount = (amount * response.conversion_rates[currency]).toFixed(2);
   $('#currency-output').text(`This amount of USD is worth ${outputAmount} ${currency}`);
+  } else {
+    $('#error-output').text(`There was an error: ${response.message}.`)
+  }
 }
 
-$(document).ready(function() {
-  $('#currency-exchange-form').submit(function(event) {
+$(document).ready(function () {
+  $('#currency-exchange-form').submit(function (event) {
     event.preventDefault();
-    let amount = $("#USD-amount").val();
-    let currency = $("#currency").val();
+    let amount = $('#USD-amount').val();
+    let currency = $('#currency').val();
     ExchangeRateService.currencyExchange()
       .then(function (response) {
-        let exchangeRate = findExchangeRate(response, currency);
-        outputExchange(amount, exchangeRate, currency);
+        outputExchange(amount, currency, response);
       });
-    $('#currency-output').append(`<p></p>`);
   });
 });
